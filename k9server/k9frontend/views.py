f5a437246ac8a7c4866153127b3ee02dc291be7e
les <nanotrippolo@gmail.com>
2015-04-26 20:07:51 +0200

    added some stuff
    -first view created and working
    -fixed settings.py
    -added requirements.txt

--- k9server/k9frontend/views.py
+++ k9server/k9frontend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.http import HttpResponse
+from ajaxutils.decorators import ajax
+from django.db.models import Count
+
+def index(request):
+    return render_to_response("index.html", context_instance=RequestContext(request))
+
 # Create your views here.
--- k9server/k9server/urls.py
+++ k9server/k9server/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # Examples:
@@ -8,3 +9,12 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += patterns('k9frontend.views',
+                        url(r'^$', 'index', name='index'),
+                        #url(r'^search/(?P<extension_name>\S+)/$', 'search_extension', name='search_extension'),
+                        #url(r'^search/(?P<program>\w+)/$', 'search_program', name='search_program'),
+                        #url(r'^top/$', 'get_top_results', name='get_top_results'),
+
+                        )
+urlpatterns += staticfiles_urlpatterns()
