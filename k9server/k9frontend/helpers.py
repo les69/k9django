f3c4ac52aa1082a5f6ca5d4ae579ea8201bc9350
les <nanotrippolo@gmail.com>
2015-06-17 21:08:39 +0200

    updated wrong requirements

--- k9server/k9frontend/helpers.py
+++ k9server/k9frontend/helpers.py
@@ -5,6 +5,33 @@ from k9models.models import Message
 from dateutil.parser import parse
 
 
+def get_daily_polarity(date):
+
+    m_list = Message.objects.filter(recv_date__year=date.year, recv_date__month=date.month, recv_date__day=date.day)
+    tot_polarity = 0
+
+    for message in m_list:
+        tot_polarity += get_polarity(message.message_text)
+
+    return tot_polarity/len(m_list)
+
+def get_daily_subjectivity(date):
+
+    m_list = Message.objects.filter(recv_date__year=date.year, recv_date__month=date.month, recv_date__day=date.day)
+    tot_subj = 0
+
+    for message in m_list:
+        tot_subj += get_polarity(message.message_text)
+
+    return tot_subj/len(m_list)
+
+
+
+def get_subjectivity(phrase):
+    blob = TextBlob(phrase)
+    return blob.subjectivity
+
+
 def get_polarity(phrase):
     blob = TextBlob(phrase)
     return blob.polarity
